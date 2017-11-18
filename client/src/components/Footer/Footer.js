c51a09f98629799ac6ed6b215e276764475feb7b
unknown <ratlarva@gmail.com>
2017-11-18 11:36:56 -0700

    footer-transparency

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,32 +4,33 @@ import CSS from "./footer.css";
  const Footer = () => 
   <footer style={{ height: 300 }} class="page-footer">
     <h1> </h1>
-    <div class="row">
+    <div>
+    	<div class="row" id="picture-container">
     	<div class="column">
     		<ul>
-    			<li><a href="http://www.zorb.cn/">Jobs</a></li>
-    			<li><a href="http://www.zorb.cn/">Get a Zorb!</a></li>
-    			<li><a href="http://www.zorb.cn/">Contact Info</a></li>
-    			<li><a href="http://www.zorb.cn/">Entropy in the Universe</a></li>
+    			<li><a class="footer-links" href="http://www.zorb.cn/">Jobs</a></li>
+    			<li><a class="footer-links" href="http://www.zorb.cn/">Get a Zorb!</a></li>
+    			<li><a class="footer-links" href="http://www.zorb.cn/">Contact Info</a></li>
+    			<li><a class="footer-links" href="http://www.zorb.cn/">Entropy in the Universe</a></li>
     		</ul>
     	</div>
     	<div class="column">
     		<ul>
-    			<li><a href="http://www.zorb.cn/">About Us</a></li>
-    			<li><a href="http://www.zorb.cn/">Contact</a></li>
-    			<li><a href="http://www.zorb.cn/">Privacy Policy</a></li>
-    			<li><a href="http://www.zorb.cn/">Social Media</a></li>
-    		</ul>
+    			<li><a class="footer-links" href="http://www.zorb.cn/">About Us</a></li>
+    			<li><a class="footer-links" href="http://www.zorb.cn/">Contact</a></li>
+    			<li><a class="footer-links" href="http://www.zorb.cn/">Privacy Policy</a></li>
+    			<li><a class="footer-links" href="http://www.zorb.cn/">Social Media</a></li>
+    		</ul> 
     	</div>
     	<div class="column">
     		<ul>
-    			<li><a href="http://www.zorb.cn/">Boring Legal Stuff</a></li>
-    			<li><a href="http://www.popularmechanics.com/home/how-to-plans/">Learn About DIY Mechanics</a></li>
-    			<li><a href="http://www.zorb.cn/">Become an Affiliate</a></li>
-    			<li><a href="http://www.zorb.cn/">Join the Club</a></li>
+    			<li><a class="footer-links" href="http://www.zorb.cn/">Boring Legal Stuff</a></li>
+    			<li><a class="footer-links" href="http://www.popularmechanics.com/home/how-to-plans/">Learn About DIY Mechanics</a></li>
+    			<li><a class="footer-links" href="http://www.zorb.cn/">Become an Affiliate</a></li>
+    			<li><a class="footer-links" href="http://www.zorb.cn/">Join the Club</a></li>
     		</ul>
     	</div>
-       
+       </div>
   	</div>
  </footer>
 
