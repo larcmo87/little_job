d51a96891e358f4fd73ca2a21ec06396291301f2
Robert Morris <larcmo@gmail.com>
2017-11-28 16:55:23 -0700

    added nav bar functionality

--- client/src/NavNavigation.js
+++ client/src/NavNavigation.js
@@ -1,11 +1,29 @@
-let TypeNavigation = "";
-let NavigateTo = "";
+let typeNavigation = "";
+let navigateTo = "";
+let activeLink = "";
 
 
 export function getNavType(){
-	return TypeNavigation;
-}
+	return typeNavigation;
+};
 
 export function setNavType(type){
-	TypeNavigation = type;
+	typeNavigation = type;
+};
+
+export function getNavPath(){
+		return navigateTo;
+};
+
+export function setNavPath(path){
+		navigateTo = path;
+};
+
+export function getActive(){
+	return activeLink;
+};
+
+export function setActive(link){
+	activeLink = link;
 }
+
